70dc9af4ba3200351c95a8cf812e59b45cb6b2df
Eduardo Higashi <eduardo.higashi.dev@gmail.com>
2021-04-21 11:36:01 -0300

    Add axios and Home typage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,37 @@
-export default function Home(props) {
+import { GetStaticProps } from "next";
+import { api } from "../services/api";
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+  thumbnail: string;
+  description: string;
+  file: {
+    url: string;
+    type: string;
+    duration: number;
+  };
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
   console.log(props.episodes);
   return <h1>Index</h1>;
 }
 
-export async function getStaticProps() {
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("/episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
 
   return {
     props: {
@@ -13,4 +39,4 @@ export async function getStaticProps() {
     },
     revalidate: 60 * 60 * 8, //60 segundos x 60 minutos x 8 horas
   };
-}
+};
