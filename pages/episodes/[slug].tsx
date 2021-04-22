e4bec730437b260a1b25f72611ae8d9416d4d658
Eduardo Higashi <eduardo.higashi.dev@gmail.com>
2021-04-22 18:43:11 -0300

    Add pre-render on Episodes page, so it caches last 2 episodes pages

--- pages/episodes/[slug].tsx
+++ pages/episodes/[slug].tsx
@@ -59,8 +59,24 @@ export const Episodes = ({ episode }: EpisodeProps) => {
 };
 
 export async function getStaticPaths() {
+  const { data } = await api.get("/episodes", {
+    params: {
+      _limit: 2,
+      _sort: "published_at",
+      _order: "desc",
+    },
+  });
+
+  const paths = data.map((episode) => {
+    return {
+      params: {
+        slug: episode.id,
+      },
+    };
+  });
+
   return {
-    paths: [],
+    paths,
     fallback: "blocking",
   };
 }
