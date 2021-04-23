2616d583ea9f8d8866be2edc61b326333b81f67c
Eduardo Higashi <eduardo.higashi.dev@gmail.com>
2021-04-22 21:36:21 -0300

    Add rc-slider package and style Player slider

--- components/Player/index.tsx
+++ components/Player/index.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { useContext } from "react";
 import { PlayerContext } from "../../contexts/PlayerContext";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 import styles from "./styles.module.scss";
 
 export const Player = () => {
@@ -10,7 +12,7 @@ export const Player = () => {
     <div className={styles.playerContainer}>
       <header>
         <img src="/playing.svg" alt="Tocando agora" />
-        <strong>Tocando agora {episode?.title}</strong>
+        <strong>Tocando agora</strong>
       </header>
 
       {episode ? (
@@ -34,25 +36,37 @@ export const Player = () => {
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: "#04d361" }}
+                railStyle={{ backgroundColor: "#9f75ff" }}
+                handleStyle={{ backgroundColor: "#04d361", borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
 
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="Shuffle" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="Previous" />
           </button>
-          <button type="button" className={styles.playButton}>
+          <button
+            type="button"
+            disabled={!episode}
+            className={styles.playButton}
+          >
             <img src="/play.svg" alt="Play" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" alt="Next" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" alt="Repeat" />
           </button>
         </div>
