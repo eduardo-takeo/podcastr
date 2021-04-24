a879ad30c8e6b9810efe47c4ae9f9111acf1d7f1
Eduardo Higashi <eduardo.higashi.dev@gmail.com>
2021-04-24 19:24:19 -0300

    Change page titles

--- pages/episodes/[slug].tsx
+++ pages/episodes/[slug].tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import Head from "next/head";
 import { api } from "../../services/api";
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
@@ -30,6 +31,10 @@ export const Episodes = ({ episode }: EpisodeProps) => {
 
   return (
     <div className={styles.episode}>
+      <Head>
+        <title>{episode.title} | Podcastr</title>
+      </Head>
+
       <div className={styles.thumbnailContainer}>
         <Link href="/">
           <button type="button">
